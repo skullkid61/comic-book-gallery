ffae1f576a6070ad2b193e6526b3279246ce6051
skullkid61 <43558378+skullkid61@users.noreply.github.com>
2019-06-19 21:18:05 +0930

    Added Comic Book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the Comic Book's controller!");
+            return View();
         }
     }
 }
